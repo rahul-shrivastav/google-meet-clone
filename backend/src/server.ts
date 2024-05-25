d03bcb0e444a291b7cce09f2ef78a97cb14a6404
rahul shrivastava <rahulshrivastav052@gmail.com>
2024-05-25 19:58:36 +0530

    socket added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,12 @@ const port = process.env.PORT
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:3000"],
+        methods: ["GET", "POST"],
+    },
+});
 
 app.get('/', (req, res) => {
     res.send("server on");
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { SocketProvider } from '../context/SocketContext.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <SocketProvider>
+
+        <App />
+      </SocketProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
